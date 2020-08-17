18aca91dca6bdc0dc34c42db87961878b5a5d9e8
tameplus <tameplus@gmail.com>
2020-08-17 11:46:49 +0700

    update

--- src/modules/introduction/controller.js
+++ src/modules/introduction/controller.js
@@ -1,6 +1,8 @@
-const model = require("./model");
+const IntroductionRepository = require("./repository");
+const introRepo = new IntroductionRepository();
 module.exports = {
     list_introduction: async (query) => {
-        return await model.list_introduction(query);
+        let listData =  await introRepo.list_introduction();
+        return {status: true, message: 'success', code: 0, data: listData};
     }
 }
